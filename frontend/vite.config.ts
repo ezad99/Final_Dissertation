8442f7934feb40a7fc199400a3605ac63a1629b5
Ezad99 <ezadhaziq99@gmail.com>
2025-01-22 17:35:47 +0000

    Added url to vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["https://gurujava-frontend.onrender.com/"],
     host: '0.0.0.0',
     port: 4173,
   }
