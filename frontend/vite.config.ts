64c317d49e2e13db406a3f56d525f63a8a53ba8d
Ezad99 <ezadhaziq99@gmail.com>
2025-01-30 17:52:13 +0000

    Revert vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["gurujava-frontend.onrender.com", "https://gurujava.onrender.com/"],
+    allowedHosts: ["gurujava-frontend.onrender.com"],
     host: '0.0.0.0',
     port: 4173,
   }
